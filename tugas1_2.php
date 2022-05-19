552bb12490d83b45d5b72e4fae8171517d51bc90
mohfarhann <90471536+mohfarhann@users.noreply.github.com>
2022-05-19 14:02:18 +0700

    Add files via upload

---
+++ tugas1_2.php
@@ -0,0 +1,51 @@
+<html>
+  <body>
+      <?php
+
+        $x1 = "Cireng";
+        $x2 = "Es Duren";
+        $x3 = "Es Milo";
+        $x4 = "Es Teh";
+        $x5 = "Piscok";
+
+        $y1 = 1000;
+        $y2 = 15000;
+        $y3 = 20000;
+        $y4 = 5000;
+
+      ?>
+      <h1>Daftar Menu</h1>
+      <table border="1">
+          <tr>
+              <td>No</td>
+              <td>Nama Menu</td>
+              <td>Harga</td>
+          </tr>
+          <tr>
+              <td>1</td>
+              <td><?php echo $x1 ?></td>
+              <td><?php echo $y1 ?></td>
+          </tr>
+          <tr>
+              <td>2</td>
+              <td><?php echo $x2 ?></td>
+              <td><?php echo $y2 ?></td>
+          </tr>
+          <tr>
+              <td>3</td>
+              <td><?php echo $x3 ?></td>
+              <td><?php echo $y3 ?></td>
+          </tr>
+          <tr>
+              <td>4</td>
+              <td><?php echo $x4 ?></td>
+              <td><?php echo $y4 ?></td>
+          </tr>
+          <tr>
+              <td>5</td>
+              <td><?php echo $x5 ?></td>
+              <td><?php echo $y1 ?></td>
+          </tr>
+      </table>
+  </body>
+</html>
